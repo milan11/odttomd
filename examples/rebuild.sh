33f205f693edc5f6a85c5eb16912a6f634978d93
Milan Laslop <milan33@gmail.com>
2014-09-17 19:32:02 +0200

    examples

---
+++ examples/rebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -u
+set -e
+
+ODTTOMD_PATH="../../odttomd-build/debug/odttomd"
+
+for ODT_FILE in *.odt
+do
+	FILE_NAME=$(basename "${ODT_FILE}" ".odt")
+	"${ODTTOMD_PATH}" "${ODT_FILE}" > "${FILE_NAME}.md"
+done
+
